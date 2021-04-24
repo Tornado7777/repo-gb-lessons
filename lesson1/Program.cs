09744cc93bee3001fbef153750b2c10d95f794ea
Ivan <tornadod@mail.ru>
2021-04-24 18:34:08 +0300

    Ending

---
+++ lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста введите ваше имя:");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}");
+            Console.ReadLine();
+
+        }
+    }
+}
